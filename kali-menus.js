58fa421f00ad739231102dd98bb976e05d4f951f
Tristan Mendoza <tristanm.tx@gmail.com>
2018-05-28 22:20:11 -0500

    gjsfikisty
    ludtyuldtyo

---
+++ kali-menus.js
@@ -0,0 +1,210 @@
+
+--------------Main items
+
+InfoGath_Menu[24]='<a class="whitelink" href="https://bitbucket.org/LaNMaSteR53/recon-ng">Recon-ng</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://sparta.secforce.com/">Sparta</A>'
+
+
+
+
+-------------DNS Analysis submenu
+InfoGath_Menu[24]='<a class="whitelink" href="http://mor-pah.net/software/dmitry-deepmagic-information-gathering-tool/">DMitry</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://github.com/fwaeytens/dnsenum">DNSEnum</A>' 
+InfoGath_Menu[24]='<a class="whitelink" href="http://code.google.com/p/dnsmap/">DNSMap</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://github.com/darkoperator/dnsrecon">DNSRecon</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="http://freshmeat.net/projects/dnstracer">DNSTracer</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://sourceforge.net/projects/dnswalk/">DNSWalk</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://github.com/mschwager/fierce">Fierce (locate non-contig IP)</A>'
+-------------IDS/IPS Identification submenu
+InfoGath_Menu[24]='<a class="whitelink" href="http://www.monkey.org/~dugsong/fragroute/">Fragroute - Fragtest</A>'  
+InfoGath_Menu[24]='<a class="whitelink" href="http://www.anzen.com/research/nidsbench/fragrouter.html">Fragrouter - IDS evasion toolkit</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="http://inversepath.com/ftester.html">FTest</A>'   
+InfoGath_Menu[24]='<a class="whitelink" href="https://github.com/craig/ge.mine.nu/tree/master/lbd">LBD (load balancing detector)</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://github.com/EnableSecurity/wafw00f">WAFw00f- WAF Detection Tool</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="http://www.nta-monitor.com/wiki/index.php/Ike-scan_User_Guide">IKE_Scan</A>'
+------------Live Host Id/ Network and Port Scanners submenus
+InfoGath_Menu[24]='<a class="whitelink" href="https://github.com/ThomasHabets/arping">Arping</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://sourceforge.net/p/netdiscover/wiki/Home/">NetDiscover - ARP scanner</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="http://github.com/Zapotek/cdpsnarf">CDPSnarf</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="http://fping.org/">Fping</A>' 
+InfoGath_Menu[24]='<a class="whitelink" href="http://www.hping.org/">Hping</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://github.com/robertdavidgraham/masscan">Masscan</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://github.com/offensive-security/masscan-web-ui">">Masscan WebUI (Cyborg)</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://www.joedog.org/dick-manual/">Dick portscanner</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="http://code.google.com/p/mirandaupnptool/">Miranda (for UPnP)</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://nmap.org/download.html">ZenMap-Nmap-Ncat-Nping</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://sourceforge.net/p/dnmap/wiki/Home/">DNmap - Distributed NMap</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://github.com/vanhauser-thc/thc-ipv6">THC IPv6 attack toolkit</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://github.com/dneufeld/unicornscan">UnicornScan</A>' 
+InfoGath_Menu[24]='<a class="whitelink" href="https://code.google.com/p/wol-e/">WOL-E Wake-On-LAN tools</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://sourceforge.net/projects/xprobe/">Xprobe2</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="http://lcamtuf.coredump.cx/p0f3/">pOf</A>'
+------------OSINT Analysis submenu 
+InfoGath_Menu[24]='<a class="whitelink" href="http://www.tekdefense.com/automater/">Automater</A>' 
+InfoGath_Menu[24]='<a class="whitelink" href="http://paterva.com/web6/products/casefile.php">Paterva Casefile</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://www.paterva.com/web7/">Paterva Maltego</A>'  
+InfoGath_Menu[24]='<a class="whitelink" href="https://www.geocreepy.com/">Creepy (geolocation)</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://github.com/laramies/theHarvester">theHarvester</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://digi.ninja/projects/twofi.php">twofi - Twitter Words of Interest</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://www.morningstarsecurity.com/research/urlcrazy">URLCrazy</A>'
+------------Route Analysis submenu 
+InfoGath_Menu[24]='<a class="whitelink" href="http://lcamtuf.coredump.cx/">0trace</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://code.google.com/p/intrace/wiki/intrace">intrace</A>'
+	InfoGath_Menu[24]='<a class="whitelink" href="https://sourceforge.net/p/netdiscover/wiki/Home/">NetDiscover - ARP scanner</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://github.com/tlby/netmask">netmask</A>'
+------------SMTP Analysis submenu 
+InfoGath_Menu[24]='<a class="whitelink" href="http://pentestmonkey.net/tools/user-enumeration/smtp-user-enum">smtp-user-enum</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="http://www.jetmore.org/john/code/swaks/">swaks- SMTP Swiss Army Knife</A>'
+------------SNMP Analysis submenu 
+InfoGath_Menu[24]='<a class="whitelink" href="http://s-tech.elsat.net.pl/">braa - mass SNMP scanner</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="https://github.com/trailofbits/onesixtyone">OneSixtyOne</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="http://www.nothink.org/codes/snmpcheck/index.php">SNMP-check</A>'
+------------SSL Analysis submenu 
+InfoGath_Menu[24]='<a class="whitelink" href="http://www.gremwell.com/sslcaudit_v1_0">sslcaudit</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="http://ssldump.sourceforge.net/">SSLdump</A>'
+InfoGath_Menu[24]='<a class="whitelink" href="http://www.rutschle.net/tech/sslh.shtml">sslh- "Applicative protocol multiplexer"</A>' 
+InfoGath_Menu[24]='<a class="whitelink" href="https://github.com/rbsec/sslscan">sslscan</A>' 
+InfoGath_Menu[24]='<a class="whitelink" href="https://github.com/iSECPartners/sslyze">SSLyze</A>' 
+InfoGath_Menu[24]='<a class="whitelink" href="http://www.taddong.com/en/lab.html">tlssled</A>'
+
+	GRAB FROM KALI --  irpass-ass - AS scanner
+	GRAB FROM KALI --  irpass-cdp - CDP scanner
+1. sniffers and spoofers need to go with protocol Scanners.  Some may need to go in appsec or elsehwere
+2. fuzzers are mostly voip so reoranizing similar sections similar
+3 main intro "main" items should be integrated where they belong in subsections
+
+icons:
+Enum - Recon + sniff+spoof
+WebApp ScanVuln Assess
+Password Tool
+802.11-BT-VoIP
+Exploit tools+ 
+
+
+FUZZING- LINK DOESNT WORK- GET FROM KALI - bed
+
+Cisco tools submenu - ARE THESE IN METASPLOIT BY NOW?
+LINK DOESNT WORK- GET FROM KALI cisco-auditing-tool - (CAT)
+LINK DOESNT WORK- GET FROM KALI cisco-global-exploiter (CGE)
+LINK DOESNT WORK- GET FROM KALI cisco-ocs - mass Cisco scanning tool
+LINK DOESNT WORK- GET FROM KALI copy-router-config and merge-router-config  ---- muts@offensive-security.com
+
+Link these to Kali's github if you can't find them elsewhere
+
+http://git.kali.org/gitweb/?p=packages/chntpw.git;a=summary
+
+
+
+
+
+
+
+---------------------------------The Web Application Analysis menu
+Web Crawlers/Directory Brute Force 
+WebApp_Menu[24]='<a class="whitelink" href="http://cutycapt.sourceforge.net/">cutycapt</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://sourceforge.net/projects/dirb/files/">dirb</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://sourceforge.net/projects/dirbuster/">DirBuster</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://sourceforge.net/projects/uniscan/">UniScan - RFI, LFI, RCE scanner</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://github.com/xmendez/wfuzz">Wfuzz</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://www.httrack.com">HTtrack</A>'
+CMS & Framework Identification 
+WebApp_Menu[24]='<a class="whitelink" href="http://blindelephant.sourceforge.net/">BlindElephant</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://github.com/hatRiot/clusterd">clusterd</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://github.com/SpiderLabs/jboss-autopwn">JBoss Autopwn</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://github.com/rezasp/joomscan">OWASP JoomScan</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://code.google.com/archive/p/plecost/">Plecost</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://code.google.com/p/ua-tester/">UAtester</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://wpscan.org/">WPscan</A>'
+Web Application Proxies 
+WebApp_Menu[24]='<a class="whitelink" href="https://portswigger.net/">PortSwigger - Burp Suite</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://www.owasp.org/index.php/OWASP_Zed_Attack_Proxy_Project">OWASP ZAProxy</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://dawes.za.net/rogan/webscarab/">WebScarab</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://www.parosproxy.org">Paros proxy</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://code.google.com/archive/p/proxystrike/">ProxyStrike</A>'
+Web Vulnerability Scanners 
+WebApp_Menu[24]='<a class="whitelink" href="https://web.archive.org/web/20170918105926/http://www.webdav.org/cadaver/">caDAVer</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://github.com/hatRiot/clusterd">clusterd</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://code.google.com/p/davtest/">DAVTest</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://github.com/SpiderLabs/deblaze">Deblaze</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://tha-imax.de/git/root/fimap">fimap</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://github.com/golismero/golismero">GoLismero - The Web Knife</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://rootkit.nl/projects/lynis.html">Lynis - audit/harden *nix</A>' 
+WebApp_Menu[24]='<a class="whitelink" href="https://cirt.net/nikto2">Nikto</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://w3af.sourceforge.net/">w3af Web App Attack/Audit Framework</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://github.com/GDSSecurity/PadBuster">PadBuster</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://www.powerfuzzer.com/">Powerfuzzer</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://code.google.com/p/skipfish/">skipfish</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://wapiti.sourceforge.net/">Wapiti</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://github.com/urbanadventurer/whatweb">WhatWeb</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://xsser.03c8.net/">Cross Site Scripter (XSSer)</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://github.com/stasinopoulos/commix">CommIX (COMM)and (I)njection e(X)ploiter</A>' 
+Database Assessment
+WebApp_Menu[24]='<a class="whitelink" href="http://sqlmap.org/">SQLmap</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://code.google.com/p/jsql-injection/">jSQL</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://github.com/Neohapsis/bbqsql/">BBQSQL</A>'
+WebApp_Menu[24]='<a class="whitelink" href="https://code.google.com/p/hexorbase/">HexorBase</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://mdbtools.sourceforge.net/">MDB Tools</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://www.cqure.net/wp/tools/database/oscanner/">OScanner (for Oracle)</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://www.cqure.net/wp/tools/database/sidguesser/">SID Guesser (for Oracle)</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://ntsecurity.nu/toolbox/sqldict/">SQLDict</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://sqlninja.sourceforge.net/">SQL_Ninja</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://sqlsus.sourceforge.net/">SQLsus</A>'
+WebApp_Menu[24]='<a class="whitelink" href="http://www.red-database-security.com/scripts/oracle_security_scripts.html">tnscmd10g</A>'
+
+	GET FROM KALI -DISCONTINUED? apache-users - WebApp_Menu[24]='<a class="whitelink" href="https://labs.portcullis.co.uk/
+
+=====================================Vulnerability Analysis menu
+
+	VulnAn_Menu[24]='<a class="whitelink" href="https://nmap.org/download.html">NMAP-Zenmap-Ncat-Nping</A>'
+	VulnAn_Menu[24]='<a class="whitelink" href="https://sparta.secforce.com/">Sparta</A>'
+ 
+VulnAn_Menu[24]='<a class="whitelink" href="http://www.hackingciscoexposed.com/?link=tools">Cisco Torch</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="http://www.yersinia.net/">Yersinia</A>'
+
+VulnAn_Menu[24]='<a class="whitelink" href="http://rootkit.nl/projects/lynis.html">Lynis - audit/harden *nix</A>' 
+VulnAn_Menu[24]='<a class="whitelink" href="http://pentestmonkey.net/tools/audit/unix-privesc-check">unix-privesc-check</A>' 
+----------------------Fuzzing Tools and Stress Testing submenus
+
+VulnAn_Menu[24]='<a class="whitelink" href="https://github.com/orgcandman/Simple-Fuzzer">sfuzz- Simple Fuzzer</A>' 
+VulnAn_Menu[24]='<a class="whitelink" href="https://github.com/guilhermeferreira/spikepp">SPIKE API (chunked; listen/send TCP; send UDP)</A>'
+	VulnAn_Menu[24]='<a class="whitelink" href="https://code.google.com/archive/p/tcpjunk/">TCPJunk (Cyborg)</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="https://github.com/kamorin/DHCPig">DHCPig - DHCP exhaustion</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="https://www.joedog.org/siege-manual/">Siege- http load testing/benchmk</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="https://sourceforge.net/projects/t50/">T50 Mixed Packet Injector</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="https://github.com/azet/thc-tls-dos">THC-SSL-DOS</A>'
+
+
+
+
+
+
+
+
+
+
+
+------------------------------VoIP Tools 
+VulnAn_Menu[24]='<a class="whitelink" href="http://enumiax.sourceforge.net/">enumIAX</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="http://www.hackingexposedvoip.com/sec_tools.html">IAX Flood- VoIP flooder</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="http://www.hackingvoip.com/sec_tools.html">SIP/SDP InviteFlood</A>' 
+VulnAn_Menu[24]='<a class="whitelink" href="http://mazzoo.de/blog/2006/08/25#ohrwurm">OhrWurm</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="https://www.ee.oulu.fi/research/ouspg/PROTOS_Test-Suite_c07-sip">PROTOS-SIP</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="https://github.com/relops/rtpbreak">rtpbreak</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="http://www.hackingvoip.com/sec_tools.html">rtpflood</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="http://www.hackingvoip.com/sec_tools.html">RTPinsertsound</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="http://www.hackingvoip.com/sec_tools.html">RTPmixsound</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="http://www.p1sec.com/corp/research/tools/sctpscan/">SCTPscan</A>' 
+VulnAn_Menu[24]='<a class="whitelink" href="http://packetstormsecurity.com/files/107301/SIP-Army-Knife-Fuzzer-1123">SIP Army Knife</A>' 
+VulnAn_Menu[24]='<a class="whitelink" href="http://sipp.sourceforge.net/">SIPp- Traffic generator</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="https://sourceforge.net/projects/sipsak.berlios/">SIPsak - SIP Swiss Army Knife</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="http://voiphopper.sourceforge.net/">VoIP_Hopper</A>'
+VulnAn_Menu[24]='<a class="whitelink" href="https://github.com/EnableSecurity/sipvicious">SIPViscious: svcrack-svcrash-svmap-svreport-svwar</A> 
+
+
+
+
+
+
+
+
+
