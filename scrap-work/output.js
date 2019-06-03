5d8b7f3f2fdbd6d635dbecec28f6f8ac6bcc9af0
Tristan Mendoza <tristanm.tx@gmail.com>
2019-06-02 20:26:55 -0500

    sfHfsh
    rsuytdjyt

---
+++ scrap-work/output.js
@@ -0,0 +1,74 @@
+[0]='<span class="yellowsans">General Reference</span>'
+[1]='<a class="whitelink" href="https://www.rfc-editor.org/search/rfc_search.php">RFC Editor search</A>'
+[2]='<a class="whitelink" href="https://datatracker.ietf.org/">IETF DataTracker</A>'
+[3]='<a class="whitelink" href="https://wiki.wireshark.org/FrontPage">The Wireshark Wiki</A>'
+[4]='<a class="whitelink" href="http://www.tcpipguide.com/">TCPIPGuide.com Wiki</A>'
+[5]='<a class="whitelink" href="https://wiki.mef.net">MEF (Metro Ethernet Forum) Wiki</A>'
+[6]='<a class="whitelink" href="https://wiki.geant.org/display/public/EK/Welcome+to+the+eduPERT+Knowledge+Base">Geant PERT KB</A>
+[7]='<a class="whitelink" href="http://www.subnet-calculator.com/">Subnet-calculator.com</A>'
+[8]='<a class="whitelink" href="http://www.ascii-code.com/">ASCII-BIN-HEX-OCT Convert</a>'
+[9]='<a class="whitelink" href="https://www.iana.org/assignments/ieee-802-numbers/ieee-802-numbers.xhtml">IANA EtherType &amp; OUI #s</A>'
+[10]='<a class="whitelink" href="https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml">IANA ICMP Types &amp; Codes</A>'
+[11]='<a class="whitelink" href="https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml">IANA Internet Protocol #s</A>'
+[12]='<a class="whitelink" href="https://www.iana.org/assignments/ipv6-address-space/ipv6-address-space.xhtml">IANA IPv6 Address Space</A>'
+[13]='<a class="whitelink" href="https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.txt">IANA Transport Protocol #s</A>'
+[14]='<span class="yellowsans">Cisco Related</span>'
+[15]='<a class="whitelink" href="http://www.cisco.com/c/en/us/support/ios-nx-os-software/ios-software-release-15-6-2-t/model.html">Cisco IOS 15.6(2)T Cmd Refs</A>'
+[16]='<a class="whitelink" href="http://www.cisco.com/c/en/us/support/ios-nx-os-software/ios-software-release-15-6m-t/products-command-reference-list.html">IOS 15.6(2) Services Cmd Guides</A>' 
+[17]='<a class="whitelink" href="http://www.cisco.com/c/en/us/support/ios-nx-os-software/ios-software-release-15-6m-t/products-installation-and-configuration-guides-list.html">Cisco IOS 15.6(2)T Config Guides</A>'
+[18]='<a class="whitelink" href="http://www.cisco.com/c/en/us/support/ios-nx-os-software/ios-xe-3e/tsd-products-support-series-home.html">Cisco IOS XE 3.x Cmd Refs</a>'
+[19]='<a class="whitelink" href="http://www.cisco.com/c/en/us/support/ios-nx-os-software/ios-xr-software/tsd-products-support-series-home.html">Cisco IOS XR Cmd Refs</a>'
+[20]='<a class="whitelink" href="http://www.cisco.com/c/en/us/support/ios-nx-os-software/nx-os-software/tsd-products-support-series-home.html">Cisco IOS NX Cmd Refs</a>'
+[21]='<a class="whitelink" href="https://www.cisco.com/c/en/us/td/docs/ios-xml/ios/mpls/command/mp-cr-book.html">Cisco IOS MPLS Cmd Ref</A>'
+[22]='<a class="whitelink" href="https://sites.google.com/site/amitsciscozone/home
+[23]='<a class="whitelink" href="https://market.alcatel-lucent.com/release/jsp/sso/login.jsp">Nokia Alcatel-Lucent Portal</A>' 
+[24]='<a class="whitelink" href="https://www.arista.com/en/support/product-documentation">Arista reference docs</A>' 
+[25]='<a class="whitelink" href="https://www.arista.com/en/um-eos/eos-command-line-interface">Arista EOS CLI Reference</A>'
+[26]='<a class="whitelink" href="https://www.extremenetworks.com/support/">Extreme Networks - support and docs</A>'
+[27]='<a class="whitelink" href="https://supportforums.adtran.com">ADTRAN support</A>'
+[28]='<span class="yellowsans">AS/ BGP Lookups</span>'
+[29]='<a class="whitelink" href="https://www.bgpmon.net/">BGPMon Dashboard</A>'
+[30]='<a class="whitelink" href="http://bgp.he.net/">Hurricane Electric BGP Tools</A>'
+[31]='<a class="whitelink" href="http://www.netconfigs.com/">netconfigs.com BGP Tools</A>'
+[32]='<a class="whitelink" href="http://www.as3257.net/lg/">GTT BGP Looking Glass</A>'
+[33]='<a class="whitelink" href="https://prefix.pch.net/">Packet Clearinghouse</A>'
+[34]='<a class="whitelink" href="https://www.peeringdb.com/advanced_search">PeeringDB Adv Search</A>'
+[35]='<a class="whitelink" href="http://cyclops.cs.ucla.edu/">Cyclops network audit tool</A>'
+[36]='<a class="whitelink" href="https://www.team-cymru.org/services.html">Team Cymru IP-to-ASN, etc</A>'
+[37]='<a class="whitelink" href="https://www.findip-address.com/">Findip-address.com Lookup</A>'
+[38]='<a class="whitelink" href="https://myip.ms/">myip.ms Host Info</a>'
+[39]='<a class="whitelink" href="http://trace.die.net/">die.net TraceRoute Explorer</a>'
+[40]='<a class="whitelink" href="http://www.traceroute.org/">Traceroute.org Jumppoints</a>' 
+[41]='<span class="yellowsans">Route Servers, etc</span>'
+[42]='<a class="whitelink" href="telnet://route-server.ip.att.net">telnet://route-server.ip.att.net</A>'
+[43]='<a class="whitelink" href="telnet://route-server.rogers.com">telnet://route-server.rogers.com</A>'
+[44]='<a class="whitelink" href="telnet://route-server.cerf.net">telnet://route-server.cerf.net</A>'
+[45]='<a class="whitelink" href="telnet://route-server.videotron.net">telnet://route-server.videotron.net</A>'
+[46]='<a class="whitelink" href="http://www.traceroute.org">Public route servers list</A>'
+[47]='<a class="whitelink" href="https://www.irr.net">Internet Routing Registry (IRR)</A>'
+[48]='<a class="whitelink" href="https://irrexplorer.nlnog.net">NL NOG IRR Explorer</A>'
+[49]='<a class="whitelink" href="https://nlnog.net/tools/">NL NOG tools</A>'
+[50]=
+[51]=
+[52]=
+[53]=
+[54]=
+[55]=
+[56]=
+[57]=
+[58]=
+[59]=
+[60]=
+[61]=
+[62]=
+[63]=
+[64]=
+[65]=
+[66]=
+[67]=
+[68]=
+[69]=
+[70]=
+=
+var6"$(seq -s '] [' 1 70)"=
+$ echo "$var6"=
